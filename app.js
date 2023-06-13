54dfe27b94241a8bb6a85e074c42bec320c88149
OrianoARI <oandreoli88@gmail.com>
2023-06-13 14:55:13 +0200

    password

--- app.js
+++ app.js
@@ -28,25 +28,18 @@ app.use(function (req, res, next) {//récupère la cession utilisateur si connec
 app.use(projectRouter);
 app.use(userRouter);
 
-const start = async () => {
-    
-    if (!process.env.DB_URI) {
-        throw new Error('auth DB_URI must be defined');
-    }
-    try {
-        await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Server connected to MongoDb!');
-    } catch (err) {
-       
-        console.error(err);
-    }
 
-    const PORT = process.env.SERVER_PORT;
-    app.listen(PORT, () => {
-        console.log(`Server is listening on ${process.env.PORT}!!!!!!!!!`);
-    });
-};
-start()
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Connecté au port: ${process.env.PORT}`);
+    }
+});
+
+try {
+    mongoose.connect(process.env.DB_URI)
+    console.log("Connecté à la base de données");
+} catch (error) {
+    console.log(error);
+}
